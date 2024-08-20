d9ce1b6ecfe821d88a93b7582175593d8b9c2c81
Landon & Emma <80786423+LandonAndEmma@users.noreply.github.com>
2024-08-20 13:10:54 -0400

    Commit

--- App.Designer.cs
+++ App.Designer.cs
@@ -30,10 +30,14 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(App));
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.courselistBox = new System.Windows.Forms.ListBox();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.musiclistBox = new System.Windows.Forms.ListBox();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.weatherlistBox = new System.Windows.Forms.ListBox();
             this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.emblemlistBox = new System.Windows.Forms.ListBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -42,22 +46,37 @@
             this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.helpToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.repositoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabPage2.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.tabControl1.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.courselistBox);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(792, 424);
+            this.tabPage2.Size = new System.Drawing.Size(792, 400);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Course Filenames";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
+            // courselistBox
+            // 
+            this.courselistBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.courselistBox.FormattingEnabled = true;
+            this.courselistBox.Location = new System.Drawing.Point(3, 3);
+            this.courselistBox.Name = "courselistBox";
+            this.courselistBox.Size = new System.Drawing.Size(786, 394);
+            this.courselistBox.TabIndex = 0;
+            // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.musiclistBox);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
@@ -65,7 +84,15 @@
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Music IDs";
             this.tabPage1.UseVisualStyleBackColor = true;
-            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // musiclistBox
+            // 
+            this.musiclistBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.musiclistBox.FormattingEnabled = true;
+            this.musiclistBox.Location = new System.Drawing.Point(3, 3);
+            this.musiclistBox.Name = "musiclistBox";
+            this.musiclistBox.Size = new System.Drawing.Size(786, 394);
+            this.musiclistBox.TabIndex = 0;
             // 
             // tabControl1
             // 
@@ -82,23 +109,43 @@
             // 
             // tabPage3
             // 
+            this.tabPage3.Controls.Add(this.weatherlistBox);
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(792, 424);
+            this.tabPage3.Size = new System.Drawing.Size(792, 400);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Weather Slots";
             this.tabPage3.UseVisualStyleBackColor = true;
             // 
+            // weatherlistBox
+            // 
+            this.weatherlistBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.weatherlistBox.FormattingEnabled = true;
+            this.weatherlistBox.Location = new System.Drawing.Point(3, 3);
+            this.weatherlistBox.Name = "weatherlistBox";
+            this.weatherlistBox.Size = new System.Drawing.Size(786, 394);
+            this.weatherlistBox.TabIndex = 0;
+            // 
             // tabPage4
             // 
+            this.tabPage4.Controls.Add(this.emblemlistBox);
             this.tabPage4.Location = new System.Drawing.Point(4, 22);
             this.tabPage4.Name = "tabPage4";
             this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage4.Size = new System.Drawing.Size(792, 424);
+            this.tabPage4.Size = new System.Drawing.Size(792, 400);
             this.tabPage4.TabIndex = 3;
             this.tabPage4.Text = "Emblem Filenames";
             this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // emblemlistBox
+            // 
+            this.emblemlistBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.emblemlistBox.FormattingEnabled = true;
+            this.emblemlistBox.Location = new System.Drawing.Point(3, 3);
+            this.emblemlistBox.Name = "emblemlistBox";
+            this.emblemlistBox.Size = new System.Drawing.Size(786, 394);
+            this.emblemlistBox.TabIndex = 0;
             // 
             // menuStrip1
             // 
@@ -124,19 +171,19 @@
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
             this.openToolStripMenuItem.Text = "Open";
             // 
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
             this.saveToolStripMenuItem.Text = "Save";
             // 
             // saveAsToolStripMenuItem
             // 
             this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
-            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
             this.saveAsToolStripMenuItem.Text = "Save As";
             // 
             // helpToolStripMenuItem
@@ -150,15 +197,19 @@
             // 
             // helpToolStripMenuItem1
             // 
+            this.helpToolStripMenuItem1.DoubleClickEnabled = true;
             this.helpToolStripMenuItem1.Name = "helpToolStripMenuItem1";
             this.helpToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
             this.helpToolStripMenuItem1.Text = "Help";
+            this.helpToolStripMenuItem1.Click += new System.EventHandler(this.helpToolStripMenuItem1_Click);
             // 
             // repositoryToolStripMenuItem
             // 
+            this.repositoryToolStripMenuItem.CheckOnClick = true;
             this.repositoryToolStripMenuItem.Name = "repositoryToolStripMenuItem";
             this.repositoryToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.repositoryToolStripMenuItem.Text = "Repository";
+            this.repositoryToolStripMenuItem.Click += new System.EventHandler(this.repositoryToolStripMenuItem_Click);
             // 
             // App
             // 
@@ -171,7 +222,11 @@
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "App";
             this.Text = "Mario Kart DS ARM9 Editor";
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
             this.tabControl1.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -194,5 +249,9 @@
         private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem repositoryToolStripMenuItem;
+        private System.Windows.Forms.ListBox musiclistBox;
+        private System.Windows.Forms.ListBox courselistBox;
+        private System.Windows.Forms.ListBox weatherlistBox;
+        private System.Windows.Forms.ListBox emblemlistBox;
     }
 }
--- App.cs
+++ App.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1
@@ -15,11 +9,61 @@ namespace WindowsFormsApp1
         public App()
         {
             InitializeComponent();
+            // Attach the Click event handlers
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            this.repositoryToolStripMenuItem.Click += new System.EventHandler(this.repositoryToolStripMenuItem_Click);
+            this.helpToolStripMenuItem1.Click += new System.EventHandler(this.helpToolStripMenuItem1_Click);
         }
 
-        private void tabPage1_Click(object sender, EventArgs e)
+        // Event handler for "Open" menu item
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                // Set filter for .bin files
+                openFileDialog.Filter = "Binary files (*.bin)|*.bin";
+                openFileDialog.Title = "Open a Binary File";
 
+                // Show the dialog and check if a file was selected
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Get the selected file path
+                    string filePath = openFileDialog.FileName;
+
+                    // Perform actions with the selected file path
+                    MessageBox.Show($"Selected file: {filePath}", "File Selected", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    // TODO: Add code to handle the selected file (e.g., load and process the file)
+                }
+            }
+        }
+
+        // Event handler for "Repository" menu item
+        private void repositoryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Define the URL you want to open
+            string githubUrl = "https://github.com/LandonAndEmma/MKDS-ARM9-EDITOR-VS";
+
+            try
+            {
+                // Open the URL in the default web browser
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = githubUrl,
+                    UseShellExecute = true
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unable to open the URL. " + ex.Message);
+            }
+        }
+
+        // Event handler for "Help" menu item
+        private void helpToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            // Show a MessageBox with custom text
+            MessageBox.Show("This program allows you to edit many values in the arm9.bin file of Mario Kart DS.\n\n Code: Landon & Emma\n Special Thanks: Ermelber, Yami, MkDasher", "Help", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
