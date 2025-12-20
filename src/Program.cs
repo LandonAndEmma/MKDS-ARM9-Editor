990e865dad31c56fd70b49105142a4af3df5af2b
LandonAndEmma <80786423+LandonAndEmma@users.noreply.github.com>
2025-12-20 14:52:13 -0500

    Refactor program to use only 1 form
    Refactored the UX and GUI in how you no longer need to click on a list to then edit it's tedious value, you can now just edit it in the form. Also formatted and cleaned up the code and updated it for .NET 10 and got it optimized and shortened and simplified the code to prepare for the Avalonia UI port Update based on this new commit.

--- src/Program.cs
+++ src/Program.cs
@@ -1,13 +1,16 @@
-﻿namespace ARM9Editor
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ARM9Editor
 {
     internal static class Program
     {
         [STAThread]
+        [RequiresAssemblyFiles("Calls ARM9Editor.App.App()")]
         private static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new App());
+            Application.Run(new MainForm());
         }
     }
 }
